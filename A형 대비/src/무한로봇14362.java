a1b96974582d23ee5876eb35d9fdfa4764f11c5f
haeun <godgrace1030@naver.com>
2024-11-11 17:57:51 +0900

    11/11

---
+++ A형 대비/src/무한로봇14362.java
@@ -0,0 +1,7 @@
+
+public class 무한로봇14362 {
+
+	public static void main(String[] args) throws Exception{
+		
+	}
+}
---
+++ A형 대비/src/혁진이의프로그램검증1824.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 혁진이의프로그램검증1824 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		
+	}
+}
